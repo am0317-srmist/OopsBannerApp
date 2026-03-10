ac843b0d7f890817f550761d3e6459e3b130e2c2
am0317-srmist <am0317@srmist.edu.in>
2026-03-10 09:39:12 +0530

    added use case 1

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -2,5 +2,13 @@ public class OopsBanner{
   public static void main(String args[]){
     // Uc1:
     System.out.println("OOPS");
+        // uc2:
+        System.out.println("   ***       ***     ******    *****  ");
+        System.out.println(" **   **   **   **   **   **  **    * ");
+        System.out.println(" *     *   *     *   **   **  **      ");
+        System.out.println(" *     *   *     *   ******    ***    ");
+        System.out.println(" *     *   *     *   **           **  ");
+        System.out.println(" **   **   **   **   **      *    **  ");
+        System.out.println("   ***       ***     **       *****   ");
   }
 }
